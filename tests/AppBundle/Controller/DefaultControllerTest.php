cc8cf07e10d40acc2db2cce4158c7f10f0c0d2b5
Enapiuz <enapiuz@gmail.com>
2016-03-22 00:37:34 +0300

    fixed tests

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -9,10 +9,11 @@ class DefaultControllerTest extends WebTestCase
     public function testIndex()
     {
         $client = static::createClient();
+        $client->followRedirects();
 
         $crawler = $client->request('GET', '/');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+        $this->assertContains('Sign in', $crawler->filter('h2')->text());
     }
 }
