1a1cf50d5cbf0e3e436640f3de23c68aa5ed0dc7
Enapiuz <enapiuz@gmail.com>
2016-04-03 22:07:11 +0300

    Basic karma calculation output

--- src/AdminBundle/Controller/KarmaController.php
+++ src/AdminBundle/Controller/KarmaController.php
@@ -3,6 +3,7 @@
 namespace AdminBundle\Controller;
 
 use AppBundle\DBAL\Types\IdentityTypeType;
+use Domain\Person\PersonSummary;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,11 +15,21 @@ class KarmaController extends Controller
      */
     public function indexAction(Request $request)
     {
+        // TODO: make form instead of having raw request access
         // TODO: cahnge it to get all of the variants from database
         $identityTypes = IdentityTypeType::getChoices();
         $query = $request->query->get('query', '');
         $selectedIdentityType = $request->query->get('identity_type', IdentityTypeType::EMAIL);
+
+        /**
+         * @var PersonSummary
+         */
         $result = null;
+
+        if ($query) {
+            $karma = $this->get('karma');
+            $result = $karma->getPersonKarma($query, $selectedIdentityType);
+        }
         
         return $this->render('AdminBundle:Karma:index.html.twig', array(
             'query' => $query,
